f6b5ef7099742bdb15d8155940d281d9a025f8fc
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-04-25 11:04:52 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 public class Sistema{
 	public static void main(String[] args){
-		Ponto A = new.Ponto();
-		Ponto B = new.Ponto();
+		Scanner scanner = new Scanner(System.in);
 
-		A.Distancia(B);
+		Ponto pontoMaiorA, pontoMaiorB;
+		double maiorDistance;
+
+		double X = scanner.nextDouble();
+		double Y = scanner.nextDouble(); 
+		Ponto A = new Ponto(Xa,Ya);
+
+		X = scanner.nextDouble();
+		Y = scanner.nextDouble(); 
+		Ponto B = new Ponto(Xb, Yb);
+
+		maiorDistance = A.Distancia(B);
 		
+		while (A.x != 9999){
+
+			X = scanner.nextDouble();
+			Y = scanner.nextDouble();
+			Ponto B = new Ponto(X, Y);
+
+			if(A.Distancia(B) > maiorDistance){
+				pontoMaiorA = A;
+				pontoMaiorB = B;
+			}
+
+
+		}
 
 }
